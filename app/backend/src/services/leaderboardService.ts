6b30ffc463cb2a59f9ad0e1e56fb9a456fe6a5df
Igor Campos Moreira <igorcampos200@gmail.com>
2022-05-25 02:11:20 -0300

    fix: linter leaderboard

--- app/backend/src/services/leaderboardService.ts
+++ app/backend/src/services/leaderboardService.ts
@@ -1,10 +1,22 @@
-/* eslint-disable max-lines-per-function */
 import Matches from '../database/models/matches';
 import Teams from '../database/models/teams';
 import LeaderBoardInfo from '../interfaces/leaderBoard.interface';
 
 class LeaderboardService {
   private static startInfo:LeaderBoardInfo = {
+    name: '',
+    totalPoints: 0,
+    totalGames: 0,
+    totalVictories: 0,
+    totalDraws: 0,
+    totalLosses: 0,
+    goalsFavor: 0,
+    goalsOwn: 0,
+    goalsBalance: 0,
+    efficiency: 0,
+  };
+
+  private static Info:LeaderBoardInfo = {
     name: '',
     totalPoints: 0,
     totalGames: 0,
@@ -57,83 +69,98 @@ class LeaderboardService {
     return 0;
   });
 
+  public static forEachMatchesTotal = (team: Teams, matches: Matches[]) => {
+    matches.forEach((match) => {
+      const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals } = match;
+      if (team.id === homeTeam || team.id === awayTeam) {
+        this.Info.totalGames += 1;
+        if (homeTeamGoals === awayTeamGoals) {
+          this.Info.totalDraws += 1;
+        } else if (this.winner(match, team.id)) {
+          this.Info.totalVictories += 1;
+        } else {
+          this.Info.totalLosses += 1;
+        }
+        this.Info.goalsFavor += this.winnerGoal(match, team.id);
+        this.Info.goalsOwn += this.LosserGoal(match, team.id);
+      }
+    });
+  };
+
+  public static forEachMatchesAway = (team: Teams, matches: Matches[]) => {
+    matches.forEach((match) => {
+      const { awayTeam, homeTeamGoals, awayTeamGoals } = match;
+      if (team.id === awayTeam) {
+        this.Info.totalGames += 1;
+        if (homeTeamGoals === awayTeamGoals) {
+          this.Info.totalDraws += 1;
+        } else if (this.winner(match, team.id)) {
+          this.Info.totalVictories += 1;
+        } else {
+          this.Info.totalLosses += 1;
+        }
+        this.Info.goalsFavor += this.winnerGoal(match, team.id);
+        this.Info.goalsOwn += this.LosserGoal(match, team.id);
+      }
+    });
+  };
+
+  public static forEachMatchesHome = (team: Teams, matches: Matches[]) => {
+    matches.forEach((match) => {
+      const { homeTeam, homeTeamGoals, awayTeamGoals } = match;
+      if (team.id === homeTeam) {
+        this.Info.totalGames += 1;
+        if (homeTeamGoals === awayTeamGoals) {
+          this.Info.totalDraws += 1;
+        } else if (this.winner(match, team.id)) {
+          this.Info.totalVictories += 1;
+        } else {
+          this.Info.totalLosses += 1;
+        }
+        this.Info.goalsFavor += this.winnerGoal(match, team.id);
+        this.Info.goalsOwn += this.LosserGoal(match, team.id);
+      }
+    });
+  };
+
   public static totalBoard(teams: Teams[], matches: Matches[]) {
     const result = teams.map((team) => {
-      const info = { ...this.startInfo };
-      info.name = team.teamName;
-      matches.forEach((match) => {
-        const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals } = match;
-        if (team.id === homeTeam || team.id === awayTeam) {
-          info.totalGames += 1;
-          if (homeTeamGoals === awayTeamGoals) {
-            info.totalDraws += 1;
-          } else if (this.winner(match, team.id)) {
-            info.totalVictories += 1;
-          } else {
-            info.totalLosses += 1;
-          }
-          info.goalsFavor += this.winnerGoal(match, team.id);
-          info.goalsOwn += this.LosserGoal(match, team.id);
-        }
-      });
-      info.totalPoints = (info.totalVictories * 3) + info.totalDraws;
-      info.goalsBalance = info.goalsFavor - info.goalsOwn;
-      info.efficiency = Number(((info.totalPoints / (info.totalGames * 3)) * 100).toFixed(2));
-      return info;
+      this.Info = { ...this.startInfo };
+      this.Info.name = team.teamName;
+      this.forEachMatchesTotal(team, matches);
+      this.Info.totalPoints = (this.Info.totalVictories * 3) + this.Info.totalDraws;
+      this.Info.goalsBalance = this.Info.goalsFavor - this.Info.goalsOwn;
+      this.Info.efficiency = Number(((this.Info.totalPoints / (this.Info.totalGames * 3)) * 100)
+        .toFixed(2));
+      return this.Info;
     });
     return result;
   }
 
   public static totalBoardHome(teams: Teams[], matches: Matches[]) {
     const result = teams.map((team) => {
-      const info = { ...this.startInfo };
-      info.name = team.teamName;
-      matches.forEach((match) => {
-        const { homeTeam, homeTeamGoals, awayTeamGoals } = match;
-        if (team.id === homeTeam) {
-          info.totalGames += 1;
-          if (homeTeamGoals === awayTeamGoals) {
-            info.totalDraws += 1;
-          } else if (this.winner(match, team.id)) {
-            info.totalVictories += 1;
-          } else {
-            info.totalLosses += 1;
-          }
-          info.goalsFavor += this.winnerGoal(match, team.id);
-          info.goalsOwn += this.LosserGoal(match, team.id);
-        }
-      });
-      info.totalPoints = (info.totalVictories * 3) + info.totalDraws;
-      info.goalsBalance = info.goalsFavor - info.goalsOwn;
-      info.efficiency = Number(((info.totalPoints / (info.totalGames * 3)) * 100).toFixed(2));
-      return info;
+      this.Info = { ...this.startInfo };
+      this.Info.name = team.teamName;
+      this.forEachMatchesHome(team, matches);
+      this.Info.totalPoints = (this.Info.totalVictories * 3) + this.Info.totalDraws;
+      this.Info.goalsBalance = this.Info.goalsFavor - this.Info.goalsOwn;
+      this.Info.efficiency = Number(((this.Info.totalPoints / (this.Info.totalGames * 3)) * 100)
+        .toFixed(2));
+      return this.Info;
     });
     return result;
   }
 
   public static totalBoardAway(teams: Teams[], matches: Matches[]) {
     const result = teams.map((team) => {
-      const info = { ...this.startInfo };
-      info.name = team.teamName;
-      matches.forEach((match) => {
-        const { awayTeam, homeTeamGoals, awayTeamGoals } = match;
-        if (team.id === awayTeam) {
-          info.totalGames += 1;
-          if (homeTeamGoals === awayTeamGoals) {
-            info.totalDraws += 1;
-          } else if (this.winner(match, team.id)) {
-            info.totalVictories += 1;
-          } else {
-            info.totalLosses += 1;
-          }
-          info.goalsFavor += this.winnerGoal(match, team.id);
-          info.goalsOwn += this.LosserGoal(match, team.id);
-        }
-      });
-      info.totalPoints = (info.totalVictories * 3) + info.totalDraws;
-      info.goalsBalance = info.goalsFavor - info.goalsOwn;
-      info.efficiency = Number(((info.totalPoints / (info.totalGames * 3)) * 100).toFixed(2));
-      return info;
+      this.Info = { ...this.startInfo };
+      this.Info.name = team.teamName;
+      this.forEachMatchesAway(team, matches);
+      this.Info.totalPoints = (this.Info.totalVictories * 3) + this.Info.totalDraws;
+      this.Info.goalsBalance = this.Info.goalsFavor - this.Info.goalsOwn;
+      this.Info.efficiency = Number(((this.Info.totalPoints / (this.Info.totalGames * 3)) * 100)
+        .toFixed(2));
+      return this.Info;
     });
     return result;
   }
